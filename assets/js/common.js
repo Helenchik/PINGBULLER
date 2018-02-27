1ce5539997b85dddc0ac0cc1ea7c08beb5e89800
Kochetkova Helen <80973636555t@gmail.com>
2018-02-27 02:00:49 +0200

    first commit

---
+++ assets/js/common.js
@@ -0,0 +1,5 @@
+$('.header-slider').slick({
+  dots:true,
+  prevArrow:'<button type="button" class="header-slider_arrow .header-slider_arrow--left"><i class="fas fa-chevron-left"></i></button>',
+  nextArrow:'<button type="button" class="header-slider_arrow .header-slider_arrow--right"><i class="fas fa-chevron-right"></i></button>'
+});
