0f45e925088eabb7566f9e77c39ce12a07d1a4a1
Kochetkova Helen <80973636555t@gmail.com>
2018-02-28 20:58:45 +0200

    second commit

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,9 @@
 $('.header-slider').slick({
   dots:true,
-  prevArrow:'<button type="button" class="header-slider_arrow .header-slider_arrow--left"><i class="fas fa-chevron-left"></i></button>',
-  nextArrow:'<button type="button" class="header-slider_arrow .header-slider_arrow--right"><i class="fas fa-chevron-right"></i></button>'
+  prevArrow:'<button type="button" class="header-slider_arrow header-slider_arrow--left"><i class="fas fa-chevron-left"></i></button>',
+  nextArrow:'<button type="button" class="header-slider_arrow header-slider_arrow--right"><i class="fas fa-chevron-right"></i></button>'
 });
+$('.second-slider').slick({
+  dots:true,
+  arrows: false
+    });
