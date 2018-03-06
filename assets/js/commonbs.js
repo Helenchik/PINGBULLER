c28b6afc2dc3bd124be74ae66906c17148316cab
Kochetkova Helen <80973636555t@gmail.com>
2018-03-06 16:51:38 +0200

    fourth commit bootstrap with media-try

---
+++ assets/js/commonbs.js
@@ -0,0 +1,9 @@
+$('.header-slider').slick({
+  dots:true,
+  prevArrow:'<button type="button" class="header-slider_arrow header-slider_arrow--left"><span class="glyphicon glyphicon-chevron-left"></span></button>',
+  nextArrow:'<button type="button" class="header-slider_arrow header-slider_arrow--right"><span class="glyphicon glyphicon-chevron-right"></span></button>'
+});
+$('.common-slider').slick({
+dots:true,
+arrows: false
+});
