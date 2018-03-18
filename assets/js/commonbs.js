6a8960d079e6fe3b6304f0a54cab3cdc2093d6ab
Kochetkova Helen <80973636555t@gmail.com>
2018-03-18 17:50:22 +0200

    fifth final commit with bootstrap and media requests

--- assets/js/commonbs.js
+++ assets/js/commonbs.js
@@ -1,9 +1,11 @@
 $('.header-slider').slick({
   dots:true,
   prevArrow:'<button type="button" class="header-slider_arrow header-slider_arrow--left"><span class="glyphicon glyphicon-chevron-left"></span></button>',
-  nextArrow:'<button type="button" class="header-slider_arrow header-slider_arrow--right"><span class="glyphicon glyphicon-chevron-right"></span></button>'
+  nextArrow:'<button type="button" class="header-slider_arrow header-slider_arrow--right"><span class="glyphicon glyphicon-chevron-right"></span></button>',
+  dotsClass: 'clearfix header-slider__dots'
 });
 $('.common-slider').slick({
 dots:true,
-arrows: false
+arrows:false,
+dotsClass: 'clearfix common-slider__dots'
 });
